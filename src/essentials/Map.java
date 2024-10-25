a3414e71b8bcfe36ccacd850dbc231998a6cefb2
josevic01 <162633306+josevic01@users.noreply.github.com>
2024-10-25 15:40:49 -0300

    classe para ler e salvar o mapa num arquivo bin

--- src/essentials/Map.java
+++ src/essentials/Map.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.Serializable;
 
-public class Map extends JPanel{
+public class Map extends JPanel implements Serializable{
 	
 	public static Cell[][] grid;
 	private int gridSize;
