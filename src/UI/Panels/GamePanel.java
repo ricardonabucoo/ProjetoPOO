947cca4da77ba816b67dff076868d52085091e60
lucascalumbi <lucascalumbi30@gmail.com>
2024-10-28 13:23:29 -0300

    pequenas alteracoes

--- src/UI/Panels/GamePanel.java
+++ src/UI/Panels/GamePanel.java
@@ -1,61 +1,33 @@
 package UI.Panels;
 
+import essentials.Match;
+
 import javax.swing.*;
 
 import java.awt.*;
 
 public class GamePanel extends JPanel {
-	
 
-	    private JPanel leftPanel;
-	    private JPanel rightPanel;
-	    private JPanel centerPanel;
-	    private JPanel topPanel;
-
-	    public GamePanel() {
+	    public GamePanel(Match match) {
 	        setLayout(new BorderLayout());
 	        setBackground(Color.decode("#008b8b"));
-	        leftPanel = createLeftPanel();
-	        rightPanel = createRightPanel();
-	        centerPanel = new CreateMapPanel();
-	        topPanel = createTopPanel();
-	        add(leftPanel, BorderLayout.WEST);
-	        add(rightPanel, BorderLayout.EAST);
-	        add(centerPanel, BorderLayout.CENTER);
-	        add(topPanel, BorderLayout.NORTH);
+	        add(createPanel(), BorderLayout.WEST);
+	        add(createPanel(), BorderLayout.EAST);
+	        add(match.getMap(), BorderLayout.CENTER);
+	        add(new TopGameBoard(match), BorderLayout.NORTH);
 
 	    }
 
-	    private JPanel createLeftPanel() {
+	    private JPanel createPanel() {
 	        JPanel panel = new JPanel();
-	        panel.setBackground(Color.MAGENTA);
+	        panel.setBackground(Color.lightGray);
 	        return panel;
 	    }
-	    private JPanel createRightPanel() {
-	        JPanel panel = new JPanel();
-	        panel.setBackground(Color.CYAN);
-	        return panel;
-	    }
+
 	    private JPanel createCenterPanel() {
 	        JPanel panel = new JPanel();
 	        panel.setBackground(Color.GREEN);
 	        return panel;
 	    }
-	    private JPanel createTopPanel() {
-	        JPanel panel = new JPanel();
-	        panel.setBackground(Color.orange);
-	        return panel;
-	    }
 
-	    public static void main(String[] args) {
-	        JFrame frame = new JFrame();
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setSize(800, 600);
-	        frame.setLocationRelativeTo(null);
-	        frame.setVisible(true);
-
-	        GamePanel panel = new GamePanel();
-	        frame.add(panel);
-
-	    }
 }
