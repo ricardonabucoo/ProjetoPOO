327e4ae938689d44991247220424a4ba946c5c90
lucascalumbi <lucascalumbi30@gmail.com>
2024-10-28 13:38:43 -0300

    modificacoes simples

--- src/UI/Panels/GamePanel.java
+++ src/UI/Panels/GamePanel.java
@@ -8,26 +8,34 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
 
-	    public GamePanel(Match match) {
-	        setLayout(new BorderLayout());
-	        setBackground(Color.decode("#008b8b"));
-	        add(createPanel(), BorderLayout.WEST);
-	        add(createPanel(), BorderLayout.EAST);
-	        add(match.getMap(), BorderLayout.CENTER);
-	        add(new TopGameBoard(match), BorderLayout.NORTH);
+	private JPanel topBoard;
+	private JPanel leftPanel;
+	private PlayerInfoPanel player1Info;
+	private PlayerInfoPanel player2Info;
 
-	    }
+	public GamePanel(Match match) {
+		setLayout(new BorderLayout());
+		setBackground(Color.decode("#008b8b"));
+		leftPanel = createPanel();
+		player1Info = createPanel();
+		player2Info = createPanel();
+		add(leftPanel, BorderLayout.WEST);
+		add(rightPanel, BorderLayout.EAST);
+		add(match.getMap(), BorderLayout.CENTER);
+		add(new TopGameBoard(match), BorderLayout.NORTH);
 
-	    private JPanel createPanel() {
-	        JPanel panel = new JPanel();
-	        panel.setBackground(Color.lightGray);
-	        return panel;
-	    }
+	}
 
-	    private JPanel createCenterPanel() {
-	        JPanel panel = new JPanel();
-	        panel.setBackground(Color.GREEN);
-	        return panel;
-	    }
+	private JPanel createPanel() {
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.lightGray);
+		return panel;
+	}
+
+	private JPanel createCenterPanel() {
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GREEN);
+		return panel;
+	}
 
 }
--- src/essentials/Match.java
+++ src/essentials/Match.java
@@ -4,18 +4,23 @@ import elements.Bag;
 import elements.PassionFruitFactory;
 import elements.Player;
 
+import java.awt.*;
+
 public class Match {
 
     private Map map;
     private Player player1;
     private Player player2;
     private PassionFruitFactory passionFruitFactory;
+    private int roundCount;
 
     public Match(Map map, Player player1, Player player2, PassionFruitFactory passionFruitFactory) {
         this.map = map;
         this.player1 = player1;
         this.player2 = player2;
         this.passionFruitFactory = passionFruitFactory;
+        this.roundCount = 0;
+
     }
 
     public Player getPlayerOne() {
@@ -29,5 +34,15 @@ public class Match {
     public Map getMap() {
         return this.map;
     }
+
+    public int getRoundCount() {
+        return this.roundCount;
+    }
+
+
+
+
+
+
 
 }
