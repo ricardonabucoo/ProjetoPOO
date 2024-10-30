a1d56da50e78d5c1f1649e3b7ca1c75d8d128ba4
ThiLizard <thiagomma.lizard@gmail.com>
2024-10-30 01:05:44 -0300

    Melhorias gráficas

--- src/UI/Panels/MainMenuPanel.java
+++ src/UI/Panels/MainMenuPanel.java
@@ -4,22 +4,49 @@ import UI.Buttons.CloseMainFrameButton;
 import UI.Buttons.LoadFileButton;
 import UI.Buttons.NewMatchButton;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.Serializable;
 
 public class MainMenuPanel extends JPanel implements Serializable {
+    private BufferedImage backgroundImage;
 
     public MainMenuPanel() {
 
-        setBackground(Color.decode("#008b8b"));
-        setLayout(new FlowLayout(FlowLayout.CENTER, 50, 275));
 
-        add(new NewMatchButton());
+        setBackground(new Color(0, 139, 139,0));
+        setLayout(new FlowLayout(FlowLayout.CENTER, 100, 275));
 
-        add(new LoadFileButton( "<html>Carregar<br> Partida</html>"));
 
-        add(new CloseMainFrameButton("Sair"));
+        JLabel titleLabel = new JLabel("JOGO CATA-FRUTAS");
+        Font font = new Font("Arial", Font.BOLD, 100);
+        titleLabel.setFont(font);
+        add(titleLabel);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 20)); // Espaçamento para os botões
+
+        Font fontButton = new Font("Arial", Font.BOLD, 20);
+
+        NewMatchButton newMatchButton = new NewMatchButton();
+        newMatchButton.setFont(fontButton);
+        newMatchButton.setPreferredSize(new Dimension(200, 100));
+        buttonPanel.add(newMatchButton);
+
+        LoadFileButton loadFileButton = new LoadFileButton("<html>Carregar<br> Partida</html>");
+        loadFileButton.setFont(fontButton);
+        loadFileButton.setPreferredSize(new Dimension(200, 100));
+        buttonPanel.add(loadFileButton);
+
+        CloseMainFrameButton closeMainFrameButton = new CloseMainFrameButton("Sair");
+        closeMainFrameButton.setFont(fontButton);
+        closeMainFrameButton.setPreferredSize(new Dimension(200, 100));
+        buttonPanel.add(closeMainFrameButton);
+
+        add(buttonPanel);
 
     }
 
